9b4f6e18113432089e612d3ea6f953d4b40585eb
Nikolay <nikob95@mail.ru>
2021-06-04 16:03:06 +0300

    +13е задание

--- src/ru/appline/DataPractice_Task13.java
+++ src/ru/appline/DataPractice_Task13.java
@@ -1,6 +1,9 @@
 package ru.appline;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class DataPractice_Task13 {
@@ -9,6 +12,23 @@ public class DataPractice_Task13 {
         String str = in.nextLine();
        // System.out.println(str);
 
-
+        Pattern pattern = Pattern.compile("[a-zA-Z]");
+        Pattern pat1 = Pattern.compile("[0-9]");
+        Pattern pat2 = Pattern.compile("[а-яА-Я]");
+        String[] words = str.split(" ");
+        int count = 0;
+        for (String oneWord : words){
+            Matcher matcher = pattern.matcher(oneWord);
+            if (matcher.find()) {
+                Matcher matcher1 = pat1.matcher(oneWord);
+                Matcher matcher2 = pat2.matcher(oneWord);
+                if (!matcher1.find() && !matcher2.find()) {
+                    System.out.println(oneWord);
+                    count++;
+                }
+            }
+        }
+        System.out.printf("Всего слов на латинице: %d", count);
     }
+
 }
