0703427ce3f9bc22cb0336ba7ae6eb09e1bc54b9
Nikolay <nikob95@mail.ru>
2021-06-05 02:13:37 +0300

    фикс, 13е задание

--- src/ru/appline/DataPractice_Task13.java
+++ src/ru/appline/DataPractice_Task13.java
@@ -12,20 +12,14 @@ public class DataPractice_Task13 {
         String str = in.nextLine();
        // System.out.println(str);
 
-        Pattern pattern = Pattern.compile("[a-zA-Z]");
-        Pattern pat1 = Pattern.compile("[0-9]");
-        Pattern pat2 = Pattern.compile("[а-яА-Я]");
+        Pattern pattern = Pattern.compile("[a-zA-Z]+");
         String[] words = str.split(" ");
         int count = 0;
         for (String oneWord : words){
             Matcher matcher = pattern.matcher(oneWord);
-            if (matcher.find()) {
-                Matcher matcher1 = pat1.matcher(oneWord);
-                Matcher matcher2 = pat2.matcher(oneWord);
-                if (!matcher1.find() && !matcher2.find()) {
-                    System.out.println(oneWord);
-                    count++;
-                }
+            if (matcher.matches()) {
+                System.out.println(oneWord);
+                count++;
             }
         }
         System.out.printf("Всего слов на латинице: %d", count);
